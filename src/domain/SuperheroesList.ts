2ad996c552fc6c28af3167c33cebdfc5bcde1090
nico21gut <nico21.gut@gmail.com>
2026-09-08 15:14:29 -0300

    add heroes

--- src/domain/SuperheroesList.ts
+++ src/domain/SuperheroesList.ts
@@ -2,12 +2,7 @@ import type { Superhero } from "./Superhero";
 
 export class SuperheroesList {
   private characters: Superhero[] = [
-    { name: "Iron Man", side: "hero" },
-    { name: "Captain America", side: "hero" },
-    { name: "Thor", side: "hero" },
-    { name: "Thanos", side: "villain" },
-    { name: "Loki", side: "villain" },
-    { name: "Wanda", side: "villain" },
+    
   ];
 
   getHeroes(): Superhero[] {
